d1267fbd378347831ed829bb8155af039601707e
hjh <hjh690@naver.com>
2026-03-26 20:40:14 +0900

    ADD : 필수기능 STEP 3

--- src/main/java/org/zerock/Category.java
+++ src/main/java/org/zerock/Category.java
@@ -1,4 +1,36 @@
 package org.zerock;
 
-public class MenuItem {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Category {
+    private List<Product> Electronics;
+    private List<Product> Clothes;
+    private List<Product> Groceries;
+
+    Category(){
+        Electronics = new ArrayList<>();
+        Clothes = new ArrayList<>();
+        Groceries = new ArrayList<>();
+    }
+
+    public void addElectronics(Product product) {
+        Electronics.add(product);
+    }
+    public void addClothes(Product product) {
+        Clothes.add(product);
+    }
+    public void addGroceries(Product product) {
+        Groceries.add(product);
+    }
+
+    public  List<Product> getElectronics() {
+        return Electronics;
+    }
+    public List<Product> getClothes() {
+        return Clothes;
+    }
+    public List<Product> getGroceries() {
+        return Groceries;
+    }
 }
--- src/main/java/org/zerock/Main.java
+++ src/main/java/org/zerock/Main.java
@@ -1,24 +1,22 @@
 package org.zerock;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<Product> products = new ArrayList<Product>();
-        CommerceSystem commerceSystem = new CommerceSystem(products, scanner);
+        Category category = new Category();
+        CommerceSystem commerceSystem = new CommerceSystem(category, scanner);
 
         Product product1 = new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 5);
         Product product2 = new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 4);
         Product product3 = new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북",5);
         Product product4 = new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 7);
 
-        products.add(product1);
-        products.add(product2);
-        products.add(product3);
-        products.add(product4);
+        category.addElectronics(product1);
+        category.addElectronics(product2);
+        category.addElectronics(product3);
+        category.addElectronics(product4);
 
         commerceSystem.start();
     }
